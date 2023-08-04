0d5cd17d75b73949e1cf6ee6c6b273b9f5f08561
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-08-05 01:53:44 +0500

    Add Slider in Main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import Products from '../data';
+import './css/home.css';
+import 'swiper/css/navigation';
 
 const Home = () => (
-  <div>
-    <h1>Home Page Under Construction</h1>
-  </div>
+  <>
+    <Swiper
+      navigation
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {Products.map((product) => (
+        <SwiperSlide className="products-info" key={product.id}>
+          <img className="photo" src={product.photo} alt="productphoto" />
+        </SwiperSlide>
+      ))}
+
+    </Swiper>
+  </>
 );
 
 export default Home;
